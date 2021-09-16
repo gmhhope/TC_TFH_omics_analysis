f90e719fc82fc282daf5b15454571207d39afb8e
Minghao Gong <gmhhope@gmail.com>
2021-09-15 22:05:07 -0400

    Fix Figure 1D gene set overlap test

--- analyses/Fig1_TranscriptomeCrossPlatform/GeneSet_Overlap_analysis_Fig1D/scripts/GSEAoverlap_dot_plot_R.R
+++ analyses/Fig1_TranscriptomeCrossPlatform/GeneSet_Overlap_analysis_Fig1D/scripts/GSEAoverlap_dot_plot_R.R
@@ -1,8 +1,8 @@
-df = read.csv("./input/canonical_pathway_padj0.05_FC1.5.csv")
+df = read.csv("./output/combn/combined2dotplot.csv")
 df$log10padj = -log(df$FDR.q.value,10)
-df$group = "overlapped_mArray_RNAseq"
 df$size = as.numeric(df$X..Genes.in.Overlap..k.)
-df$Gene.Set.Name = factor(df$Gene.Set.Name, levels=unique(sorted_df$Gene.Set.Name[order(df$log10padj,decreasing = FALSE)]),ordered=TRUE)
+df$Gene.Set.Name = factor(df$Gene.Set.Name, levels = df$Gene.Set.Name)
+#df$Gene.Set.Name = factor(df$Gene.Set.Name, levels=unique(sorted_df$Gene.Set.Name[order(df$log10padj,decreasing = FALSE)]),ordered=TRUE)
 #data <- read.csv("S1.csv", sep =";", header = TRUE, stringsAsFactors = FALSE)
 library("ggplot2")
 S1 <- ggplot(df, aes(x=group, y=Gene.Set.Name, size=size, color=log10padj)) + geom_point(alpha = 0.8)  + theme_classic() #+ facet_grid(Group ~ .)
@@ -17,6 +17,6 @@ S1 = S1+scale_color_gradient2(low = "white",
                              guide = "colourbar",
                              aesthetics = "colour") #limit = c(0.000000000000000000000000000000000000000000000004, 0.03)
 S1+scale_size(range = c(2, 8))
-pdf("output/dotplot_GSEAoverlap_v3.pdf", height = 2.8, width = 6.5)
+pdf("output/GeneSetOverlap_combn_091521.pdf", height = 2.8, width = 6.5)
 print(S1)
 dev.off()
